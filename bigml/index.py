0c4fb857f6c97a22c84fa444d5f79f9bed34aa88
AndreyFriedman <andrey.fridman2001@gmail.com>
2021-10-23 16:01:02 +0300

    bigml started to work

--- bigml/index.py
+++ bigml/index.py
@@ -1,12 +1,12 @@
 from bigml.api import BigML
 
 api = BigML('kolskyron', '275ca144362abdd066a28fe126fb2f545fd5bca9')
-print('here 1')
-source = api.create_source('./data/iris.csv')
-print('here 2')
+source = api.create_source(r"C:\Users\Andrey\Documents\GitHub\cloud_project\bigml\data\iris.csv")
+print("1")
 dataset = api.create_dataset(source)
-print('here 3')
+print("2")
 model = api.create_model(dataset)
+print("3")
 prediction = api.create_prediction(model, \
-{"sepal length": 3, "sepal width": 6.2})
-api.pprint(prediction)
+    {"petal width": 1.75, "petal length": 2.45})
+    
